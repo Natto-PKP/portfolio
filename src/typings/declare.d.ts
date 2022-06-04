f16d7c86cce527a9b1ecedf348268e8716161970
Natto-PKP <wmainvielle@gmail.com>
2022-06-04 11:53:35 +0200

    add jpg file declaration

--- src/typings/declare.d.ts
+++ src/typings/declare.d.ts
@@ -3,6 +3,11 @@ declare module '*.png' {
   export default value;
 }
 
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+
 declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
