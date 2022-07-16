1a431798b107233a6f2723b071949f3dedf12026
Natto-PKP <wmainvielle@gmail.com>
2022-07-16 15:46:31 +0200

    add redis, discord.js and some projects

--- src/components/Projects/projects.component.tsx
+++ src/components/Projects/projects.component.tsx
@@ -16,6 +16,8 @@ import NodeIcon from '../../images/icons/nodejs.svg';
 import PostgresIcon from '../../images/icons/postgresql.svg';
 import ReactIcon from '../../images/icons/react.svg';
 import SassIcon from '../../images/icons/sass.svg';
+import RedisIcon from '../../images/icons/redis.svg';
+import DiscordJSIcon from '../../images/icons/discord-js.svg';
 
 const tags = [
   { name: 'Typescript', icon: TypescriptIcon },
@@ -29,6 +31,8 @@ const tags = [
   { name: 'Postgresql', icon: PostgresIcon },
   { name: 'React', icon: ReactIcon },
   { name: 'Scss / Sass', icon: SassIcon },
+  { name: 'Redis', icon: RedisIcon },
+  { name: 'Discord.js', icon: DiscordJSIcon },
 ];
 
 const projects = [
@@ -52,7 +56,7 @@ const projects = [
   {
     name: 'discord-sucrose',
     description: 'Structure et fonctions utiles pour créer facilement un bot Discord',
-    tags: ['git', 'node.js', 'typescript', 'eslint'],
+    tags: ['git', 'node.js', 'typescript', 'discord.js', 'eslint'],
     start: '22/11/2021',
     end: '02/03/2022',
     git: 'https://github.com/Natto-PKP/discord-sucrose',
@@ -61,10 +65,34 @@ const projects = [
   {
     name: 'Sophie',
     description: 'Bot discord multi-tâches',
-    tags: ['git', 'node.js', 'typescript', 'postgresql', 'eslint'],
+    tags: ['git', 'node.js', 'typescript', 'discord.js', 'postgresql', 'eslint'],
     start: '13/10/2021',
     end: '???',
     git: 'https://github.com/Natto-PKP/Sophie',
+  },
+  {
+    name: 'Shequ',
+    description: "Bot discord d'exemple pour la communauté",
+    tags: ['git', 'node.js', 'typescript', 'discord.js', 'postgresql', 'redis', 'eslint'],
+    start: '06/06/2022',
+    end: '???',
+    git: 'https://github.com/Natto-PKP/Shequ',
+  },
+  {
+    name: 'animes.api.natchy.xyz',
+    description: 'Api du projet de liste d\'animés',
+    tags: ['git', 'node.js', 'typescript', 'postgresql', 'eslint'],
+    start: '18/06/2022',
+    end: '???',
+    git: 'https://github.com/Natto-PKP/animes.api.natchy.xyz',
+  },
+  {
+    name: 'animes.natchy.xyz',
+    description: 'Web front du projet de la liste d\'animés',
+    tags: ['git', 'react', 'scss / sass', 'typescript', 'eslint'],
+    start: '15/07/2022',
+    end: '???',
+    git: 'https://github.com/Natto-PKP/animes.natchy.xyz',
   },
 ];
 
